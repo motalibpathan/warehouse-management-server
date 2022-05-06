d333d93ed879bceb4d78d223680bcbdcf9e2d5ca
motalibpathan11 <73628059+motalibpathan11@users.noreply.github.com>
2022-05-06 20:44:41 +0600

    get single  inventory by id

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.3h0ae.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
@@ -34,6 +34,30 @@ async function run() {
       }
       res.send(inventories);
     });
+    app.get("/inventory/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const inventory = await inventoryCollection.findOne(query);
+      res.send(inventory);
+    });
+
+    app.patch("/inventory/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          quantity: req.body.quantity,
+          sold: req.body.sold,
+        },
+      };
+      const options = { upsert: true };
+      const result = await inventoryCollection.updateOne(
+        filter,
+        updateDoc,
+        options
+      );
+      res.send(result);
+    });
   } finally {
   }
 }
@@ -41,7 +65,7 @@ async function run() {
 run().catch(console.dir);
 
 app.get("/", (req, res) => {
-  res.send("Hello from ware-house server!");
+  res.send("Hello from warehouse server!");
 });
 
 app.listen(port, () => {
