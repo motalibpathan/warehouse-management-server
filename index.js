5fb7e52881e04a99eac31d6aa57a646ac68ebe9f
motalibpathan11 <73628059+motalibpathan11@users.noreply.github.com>
2022-05-08 01:22:36 +0600

    JWT varification added

--- index.js
+++ index.js
@@ -6,8 +6,25 @@ const port = process.env.PORT || 5000;
 
 const app = express();
 
+// middleware
 app.use(cors());
 app.use(express.json());
+
+function verifyJWT(req, res, next) {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader) {
+    return res.status(401).send({ message: "unauthorized access" });
+  }
+  const token = authHeader.split(" ")[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(403).send({ message: "Forbidden access" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+}
 
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.3h0ae.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
@@ -45,12 +62,17 @@ async function run() {
       res.send(inventories);
     });
 
-    app.get("/inventoryByEmail", async (req, res) => {
+    app.get("/inventoryByEmail", verifyJWT, async (req, res) => {
+      const decodedEmail = req.decoded.email;
       const email = req.query.email;
-      const query = { email: email };
-      const cursor = inventoryCollection.find(query);
-      const result = await cursor.toArray();
-      res.send(result);
+      if (email === decodedEmail) {
+        const query = { email: email };
+        const cursor = inventoryCollection.find(query);
+        const result = await cursor.toArray();
+        res.send(result);
+      } else {
+        res.status(403).send({ message: "forbidden access" });
+      }
     });
 
     app.get("/inventory/:id", async (req, res) => {
